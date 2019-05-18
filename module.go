898ac01bbeb1cc4486baed2d396ac726d439fd78
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-18 17:41:36 +0100

    Group package-level variables

--- module.go
+++ module.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-05-11 16:38:49 C54F39                             zr-web/[module.go]
+// :v: 2019-05-18 17:41:25 9354FF                             zr-web/[module.go]
 // -----------------------------------------------------------------------------
 
 // Package web provides HTML generation functions
@@ -13,10 +13,12 @@ import (
 	"github.com/balacode/zr"
 )
 
-// PL is fmt.Println() but is used only for debugging.
-var PL = fmt.Println
+var (
+	// PL is fmt.Println() but is used only for debugging.
+	PL = fmt.Println
 
-// VL is zr.VerboseLog() but is used only for debugging.
-var VL = zr.VerboseLog
+	// VL is zr.VerboseLog() but is used only for debugging.
+	VL = zr.VerboseLog
+)
 
 //end
